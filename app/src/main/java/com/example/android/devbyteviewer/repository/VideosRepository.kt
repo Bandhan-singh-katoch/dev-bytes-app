3a0e847c49884fa3fdf942180e2c1d4d0daa0e17
Bandhan-singh-katoch <parbhatbandhan@gmail.com>
2021-07-21 19:33:28 +0530

    retrieving data from database

--- app/src/main/java/com/example/android/devbyteviewer/repository/VideosRepository.kt
+++ app/src/main/java/com/example/android/devbyteviewer/repository/VideosRepository.kt
@@ -1,7 +1,11 @@
 
 package com.example.android.devbyteviewer.repository
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Transformations
 import com.example.android.devbyteviewer.database.VideosDatabase
+import com.example.android.devbyteviewer.database.asDomainModel
+import com.example.android.devbyteviewer.domain.DevByteVideo
 import com.example.android.devbyteviewer.network.Network
 import com.example.android.devbyteviewer.network.asDatabaseModel
 import kotlinx.coroutines.Dispatchers
@@ -10,6 +14,10 @@ import kotlinx.coroutines.withContext
 // this repository fetches the video from the network and stores on database
 
 class VideosRepository(private val database: VideosDatabase){
+
+    val videos : LiveData<List<DevByteVideo>> = Transformations.map(database.videoDao.getVideos()){
+        it.asDomainModel()
+    }
 
     suspend fun refreshVideos(){
         withContext(Dispatchers.IO){
